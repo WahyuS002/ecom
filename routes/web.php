0b5503e0103d082cf9f6d97ecee6016ad588c83d
WahyuS002 <syahputrawahyu61@gmail.com>
2020-03-15 21:20:41 +0700

    Layout II

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function () {
+        Route::get('/', 'DashboardController@index')
+            ->name('dashboard');
+    });
